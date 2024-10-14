10f1617845a3801c31a19825c7a27d79b4653962
oyesola-dolapo <oyesolabolamide@gmail.com>
2024-10-15 00:06:35 +0100

    solutions section

--- src/Components/Home/Solutions.jsx
+++ src/Components/Home/Solutions.jsx
@@ -6,22 +6,22 @@ export default function Solutions() {
       key: 1,
       title: "Create a unified customer view",
       subtitle:
-        "understand your customer through a single, comprehensive lens. Clean, unified data gives you one source of truth that you can share across teams",
+        "understand your customer through a single, comprehensive lens. Clean, unified data gives you one source of truth that you can share across teams.",
       img: "./assets/images/unifiedView.avif",
       link: {
         title: "Unified view",
       },
+    },
+    {
+      key: 2,
+      title: "Grow lifetime value",
+      subtitle:
+        "Accelerate revenue per customer with multi-channel audience activation, churn prevention, loyalty and engagement enhancement, uplift/increment testing, improved ad targeting, and implementing next best action decisions. ",
+      img: "./assets/images/growValue.avif",
+      link: {
+        title: "Grow value",
       },
-      {
-        key: 2,
-        title: "Grow lifetime value",
-        subtitle:
-          "Accelerate revenue per customer with multi-channel audience activation, churn prevention, loyalty and engagement enhancement, uplift/increment testing, improved ad targeting, and implementing next best action decisions. ",
-        img: "./assets/images/growValue.avif",
-        link: {
-          title: "Grow value",
-        },
-      },
+    },
     {
       key: 3,
       title: "Understand customer journeys",
@@ -42,7 +42,6 @@ export default function Solutions() {
         title: "Lower costs",
       },
     },
-    
   ];
   return (
     <section className="bg-white py-8">
@@ -54,15 +53,15 @@ export default function Solutions() {
           return (
             <a
               href="#"
-              className="min-w-[90%] h-[40rem] flex flex-col justify-between">
+              className="min-w-[87%] h-[40rem] flex flex-col justify-between">
               <div>
                 <img
                   src={item.img}
                   alt=""
                   className="object-cover w-full h-[24rem]"
                 />
-                <p className="py-3 text-lg">{item.title}</p>
-                <p className="opacity-[50%]">{item.subtitle}</p>
+                <p className="py-3 text-[1.1rem]">{item.title}</p>
+                <p className="opacity-[50%] text-md">{item.subtitle}</p>
               </div>
               <a href="#" className="underline font-bold">
                 {item.link.title}
